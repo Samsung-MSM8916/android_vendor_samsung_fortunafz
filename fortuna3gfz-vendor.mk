f80105ee19ddea6c30236e7fb5508914179120bb
ZIM <zim555@mail.ru>
2016-12-11 16:16:37 +0200

    Fix path

--- fortuna3gfz-vendor.mk
+++ fortuna3gfz-vendor.mk
@@ -2,4 +2,4 @@
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortuna3gfz/overlay
 
 # Include non-open-source files
-$(call inherit-product, vendor/samsung/fortuna3gfs/fortuna3gfz-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/fortuna3gfz/fortuna3gfz-vendor-blobs.mk)
