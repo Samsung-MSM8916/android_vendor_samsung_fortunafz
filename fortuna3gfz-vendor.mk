e78327f859bbfdcb15c1a2904a1b4cf079435d59
ZIM <zim555@mail.ru>
2016-12-17 22:27:48 +0200

    Rename FZ variant

--- fortuna3gfz-vendor.mk
+++
@@ -1,5 +0,0 @@
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortuna3gfz/overlay
-
-# Include non-open-source files
-$(call inherit-product, vendor/samsung/fortuna3gfz/fortuna3gfz-vendor-blobs.mk)
---
+++ fortunafz-vendor.mk
@@ -0,0 +1,5 @@
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortunafz/overlay
+
+# Include non-open-source files
+$(call inherit-product, vendor/samsung/fortunafz/fortunafz-vendor-blobs.mk)
